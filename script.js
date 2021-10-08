08b677eb7ae842d1309aed55ea095aee779d50be
Yves Ganza <75645764+yves-ganza@users.noreply.github.com>
2021-10-08 15:51:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    const principal = document.getElementById("principal").value;
+    const rate = document.getElementById("rate").value;
+    const years = document.getElementById("years").value;
+    const interest = principal * years * rate/100;
+    const year = new Date().getFullYear() + parseInt(years);
+
+    const updateRate = (e) => {
+        document.getElementById("rate_val").innerText = e.target.value + "%";
+    }
+
+    rate.onchange = updateRate;
+
+    document.querySelector("result").innerText = `
+        If you deposit ${principal},
+        at an interest rate of ${rate}%.
+        You will receive an amount of ${interest},
+        in the year ${year}
+    `
+
+    principal.style.focus = true;
 }
-        
+          
